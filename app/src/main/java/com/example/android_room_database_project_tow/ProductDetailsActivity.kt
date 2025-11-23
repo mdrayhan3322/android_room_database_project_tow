ee8c3e9a5e3b26020fcd587a61d8c8554a379f32
Rayhan <mdrayhan260225@gmail.com>
2025-11-23 21:34:14 +0600

    room project ui code

---
+++ app/src/main/java/com/example/android_room_database_project_tow/ProductDetailsActivity.kt
@@ -0,0 +1,16 @@
+package com.example.android_room_database_project_tow
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ProductDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_product_details)
+
+    }
+}
