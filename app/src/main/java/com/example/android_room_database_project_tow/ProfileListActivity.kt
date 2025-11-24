0306ed7d56cb52cdb7e07c8324e6bdc9dc43e8c0
Rayhan <mdrayhan260225@gmail.com>
2025-11-24 22:05:25 +0600

    Profile lisst activity file

--- app/src/main/java/com/example/android_room_database_project_tow/ProfileListActivity.kt
+++ app/src/main/java/com/example/android_room_database_project_tow/ProfileListActivity.kt
@@ -1,16 +1,60 @@
 package com.example.android_room_database_project_tow
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlin.jvm.java
 
 class ProfileListActivity : AppCompatActivity() {
+
+    private lateinit var profileViewModel: UserProfileViewModel
+    private lateinit var profileAdapter: ProfileAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_profile_list)
+
+
+        profileViewModel = ViewModelProvider(this).get(UserProfileViewModel::class.java)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.profileRecyclerView)
+        profileAdapter = ProfileAdapter()
+
+        recyclerView.adapter = profileAdapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        profileViewModel.getUserProfiles().observe(this, Observer { profiles ->
+            profileAdapter.submitList(profiles)
+        })
+
+        profileAdapter.setOnItemClickListener { userProfile ->
+            val intent = Intent(this@ProfileListActivity, ProductDetailsActivity::class.java)
+            intent.putExtra("USER_PROFILE", userProfile)
+            startActivity(intent)
+        }
+
+        profileAdapter.setOnDeleteClickListener { userProfile ->
+            profileViewModel.deleteUserProfile(userProfile)
+        }
+
+        profileAdapter.setOnUpdateClickListener { userProfile ->
+            val intent = Intent(this@ProfileListActivity, UpdateProfileActivity::class.java)
+            intent.putExtra("USER_PROFILE", userProfile)
+            startActivity(intent)
+        }
+
+        findViewById<FloatingActionButton>(R.id.addProfileBtn).setOnClickListener {
+            val intent = Intent(this@ProfileListActivity, AddProfileActivitiy::class.java)
+            startActivity(intent)
+        }
 
     }
 }
