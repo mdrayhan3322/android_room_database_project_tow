5987873650287140989e97d4c4c69299acec219a
Rayhan <mdrayhan260225@gmail.com>
2025-11-24 20:36:53 +0600

    UserProfileViewModel file done

---
+++ app/src/main/java/com/example/android_room_database_project_tow/UserProfileViewModel.kt
@@ -0,0 +1,38 @@
+package com.example.android_room_database_project_tow
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+
+class UserProfileViewModel (application: Application) : AndroidViewModel(application){
+    private val repository: UserProfileRepository
+
+    init {
+        val userProfileDao = UserDatabase.getDatabase(application).userProfileDao()
+        repository = UserProfileRepository(userProfileDao)
+    }
+    fun getUserProfiles(): LiveData<List<UserProfile>> {
+        return repository.getUserProfiles()
+    }
+    fun insertUserProfile(userProfile: UserProfile) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.insert(userProfile)
+        }
+    }
+
+    fun updateUserProfile(userProfile: UserProfile) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.update(userProfile)
+        }
+    }
+
+    fun deleteUserProfile(userProfile: UserProfile) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.delete(userProfile)
+        }
+    }
+}
