59f0ec82dcd614bbe2806ea9f1bab9dd252aa938
Rayhan <mdrayhan260225@gmail.com>
2025-11-24 16:21:12 +0600

    main acvity done

--- app/src/main/java/com/example/android_room_database_project_tow/MainActivity.kt
+++ app/src/main/java/com/example/android_room_database_project_tow/MainActivity.kt
@@ -1,20 +1,26 @@
 package com.example.android_room_database_project_tow
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var listButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        listButton = findViewById(R.id.profileListBtn)
+        listButton.setOnClickListener {
+            startActivity(Intent(this, ProfileListActivity::class.java))
+            finish()
         }
+
     }
 }
