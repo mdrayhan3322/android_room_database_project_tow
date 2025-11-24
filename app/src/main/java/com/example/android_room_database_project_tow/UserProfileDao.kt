6ac5c3aca91f0c4ac97dc053286621f428e9f9ee
Rayhan <mdrayhan260225@gmail.com>
2025-11-24 18:40:29 +0600

    Dao class done

---
+++ app/src/main/java/com/example/android_room_database_project_tow/UserProfileDao.kt
@@ -0,0 +1,24 @@
+package com.example.android_room_database_project_tow
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface UserProfileDao {
+    @Insert
+//    suspend fun insert(userProfile: UserProfile)
+    suspend fun insert(userProfile: UserProfile)
+
+    @Update
+    suspend fun update(userProfile: UserProfile)
+
+    @Delete
+    suspend fun delete(userProfile: UserProfile)
+
+    @Query("SELECT * FROM user_profile")
+    fun getAllUserProfiles(): LiveData<List<UserProfile>>
+}
